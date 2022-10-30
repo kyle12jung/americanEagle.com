8aea703f38584d576c2202e2ba382e804a8374ea
Kyle Jung <kyle12jung@gmail.com>
2022-10-29 20:32:27 -0500

    slideshow fullscreen implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,15 +17,28 @@ closeMobileMenu.addEventListener('click', function() {
 const buttons = document.querySelectorAll('[data-carousel-button]');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
+        // carousel properties
         const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
         const slides = button.closest('[data-carousel]').querySelector('[data-slides]')
         const activeSlide = slides.querySelector('[data-active]')
+
+        // navigational dots properties
+        const dots = button.closest('[data-carousel]').querySelector('[data-dots]')
+        const activeDot = dots.querySelector('[data-dot-active]')
+
+        // indexing slides and dots
         let newIndex = [...slides.children].indexOf(activeSlide) + offset
         if (newIndex < 0) newIndex = slides.children.length - 1
         if (newIndex >= slides.children.length) newIndex = 0
 
+        // transition of slides
         slides.children[newIndex].dataset.active = true
         delete activeSlide.dataset.active
+
+        // transition of dots
+        dots.children[newIndex].dataset.dotActive = true
+        delete activeDot.dataset.dotActive
+        console.log(activeDot)
     })
 })
 
