9ae04723d3bf1304a2f3f5d3b2bd5c14251a9d98
Kyle Jung <kyle12jung@gmail.com>
2022-10-30 19:06:32 -0500

    everything implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,6 @@ buttons.forEach(button => {
         // transition of dots
         dots.children[newIndex].dataset.dotActive = true
         delete activeDot.dataset.dotActive
-        console.log(activeDot)
     })
 })
 
@@ -47,4 +46,55 @@ const searchButton = document.querySelector('.navbar__search-svg');
 const searchBar = document.querySelector('.navbar__search');
 searchButton.addEventListener('click', () => {
     searchBar.style.display = 'flex';
+})
+
+// mobile view footer expand/collapse fuctionality
+const minusStyle = "margin-left: 4.5px; margin-top: 8px; width: 20px; height: 20px";
+const plusStyle = "width: 30px;";
+const footerButton1 = document.querySelector('#expand-list-1');
+let footerButton1State = 0;
+footerButton1.addEventListener('click', () => {
+    if (!footerButton1State) {
+        footerButton1State = 1;
+        footerButton1.children[0].src = "/assets/images/minus.png"
+        footerButton1.children[0].style.cssText = minusStyle
+        document.querySelector('#list-1').style.display = 'block';
+    } else {
+        footerButton1State = 0;
+        footerButton1.children[0].src = "/assets/images/plus.png"
+        footerButton1.children[0].style.cssText = plusStyle
+        document.querySelector('#list-1').style.display = 'none';
+    }
+})
+
+const footerButton2 = document.querySelector('#expand-list-2');
+let footerButton2State = 0;
+footerButton2.addEventListener('click', () => {
+    if (!footerButton2State) {
+        footerButton2State = 1;
+        footerButton2.children[0].src = "/assets/images/minus.png"
+        footerButton2.children[0].style.cssText = minusStyle
+        document.querySelector('#list-2').style.display = 'block';
+    } else {
+        footerButton2State = 0;
+        footerButton2.children[0].src = "/assets/images/plus.png"
+        footerButton2.children[0].style.cssText = plusStyle
+        document.querySelector('#list-2').style.display = 'none';
+    }
+})
+
+const footerButton3 = document.querySelector('#expand-list-3');
+let footerButton3State = 0;
+footerButton3.addEventListener('click', () => {
+    if (!footerButton3State) {
+        footerButton3State = 1;
+        footerButton3.children[0].src = "/assets/images/minus.png"
+        footerButton3.children[0].style.cssText = minusStyle
+        document.querySelector('#list-3').style.display = 'block';
+    } else {
+        footerButton3State = 0;
+        footerButton3.children[0].src = "/assets/images/plus.png"
+        footerButton3.children[0].style.cssText = plusStyle
+        document.querySelector('#list-3').style.display = 'none';
+    }
 })
